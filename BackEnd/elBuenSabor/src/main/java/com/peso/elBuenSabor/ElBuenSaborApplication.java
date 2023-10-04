a55d6106025a893e5069729f97cc51353050d032
JulianeS01 <90938368+JulianeS01@users.noreply.github.com>
2023-10-04 20:07:39 -0300

    Add files via upload
    fALTA EL MAIN

--- BackEnd/elBuenSabor/src/main/java/com/peso/elBuenSabor/ElBuenSaborApplication.java
+++ BackEnd/elBuenSabor/src/main/java/com/peso/elBuenSabor/ElBuenSaborApplication.java
@@ -13,4 +13,5 @@ public class ElBuenSaborApplication {
 		// Crear instancia de Rubro
 	}
 
+
 }
--- BackEnd/elBuenSabor/src/main/java/com/peso/elBuenSabor/entities/RubroArticulo.java
+++ BackEnd/elBuenSabor/src/main/java/com/peso/elBuenSabor/entities/RubroArticulo.java
@@ -1,5 +1,6 @@
 package com.peso.elBuenSabor.entities;
 
+import ch.qos.logback.core.BasicStatusManager;
 import jakarta.persistence.*;
 import lombok.*;
 import org.antlr.v4.runtime.misc.NotNull;
@@ -35,13 +36,21 @@ public class RubroArticulo extends Base{
     @OneToMany(cascade = CascadeType.ALL,orphanRemoval = true, fetch = FetchType.EAGER)
     @JoinColumn(name = "RubroArticulo_id")
     private List<ArticuloInsumo> articuloInsumos = new ArrayList<>();
+    @OneToMany(cascade = CascadeType.ALL,orphanRemoval = true, fetch = FetchType.EAGER)
+    @JoinColumn(name = "RubroArticulo_id")
+    private List<ArticuloManufacturado> articuloManufacturados = new ArrayList<>();
 
     @ManyToOne()
     @JoinColumn(name = "id_rubro_padre")
     private RubroArticulo rubroPadre;
 
-    @OneToMany(mappedBy = "rubroPadre")
-    private List<RubroArticulo> subRubros;
+    @OneToMany(cascade = CascadeType.ALL,orphanRemoval = true, fetch = FetchType.EAGER)
+    @JoinColumn(name = "ArticuloManufacturado_id")
+    private List<DetalleArticuloManufacturado> detalleArticuloManufacturados = new ArrayList<>();
+
+    @OneToMany(cascade = CascadeType.ALL,orphanRemoval = true, fetch = FetchType.EAGER)
+    @JoinColumn(name = "ArticuloManufacturado_id")
+    private List<DetalleFactura> detalleFacturas = new ArrayList<>();
 
     public RubroArticulo(String denominacion, RubroArticulo rubroPadre) {
         this.denominacion = denominacion;
@@ -49,17 +58,41 @@ public class RubroArticulo extends Base{
     }
 
     public void setSubRubros(List<RubroArticulo> subRubros) {
-        this.subRubros.clear();
-        this.subRubros.addAll(subRubros);
+        this.setSubRubros().clear();
+        this.setSubRubros().addAll(subRubros);
     }
 
     public void agregarArticuloInsumo(ArticuloInsumo articuloInsumo) {
         articuloInsumos.add(articuloInsumo);
     }
 
+    public void agregarArticuloManufacturado(ArticuloManufacturado articuloManufacturado) {
+        articuloManufacturados.add(articuloManufacturado);
+    }
     // Faltaria:
-    // agregar ArticuloManufacturado
-    // mostrar articuloInsumo
-    // mostrar articuloManufacturado
+
+
+
+    public void mostrarArticuloInsumo() {
+        System.out.println("Los articulos Insumo de este rubro son: ");
+        for (ArticuloInsumo articuloInsumo : articuloInsumos) {
+            System.out.println("Denominacion: " + articuloInsumo.getDenominacion() + ", Modificacion: " + articuloInsumo.getFechaModificacion()
+                    +  ", Precio Compra: " + articuloInsumo.getPrecioCompra()
+                     + ", Stock Actual: " + articuloInsumo.getStockActual()
+                    + ", Fecha Alta: " + articuloInsumo.getFechaAlta() + ", Fecha Baja: " + articuloInsumo.getFechaBaja()
+                    + ", Tipo: " + articuloInsumo.getUrlImagen());
+        }
+    }
+
+    public void mostrarArticuloManufacturado() {
+        System.out.println("Los articulos Manufacturados de este rubro son: ");
+        for (ArticuloManufacturado articuloManufacturado : articuloManufacturados) {
+            System.out.println("Denominacion: " + articuloManufacturado.getDenominacion() + ", Modificacion: " + articuloManufacturado.getFechaModificacion()
+                    +  ", Tiempo Estimado: " + articuloManufacturado.getTiempoEstimadoCocina()
+                    + ", Costo : " + articuloManufacturado.getCosto()
+                    + ", Fecha Alta: " + articuloManufacturado.getFechaAlta() + ", Fecha Baja: " + articuloManufacturado.getFechaBaja()
+                    + ", Tipo: " + articuloManufacturado.getUrlImagen());
+        }
+    }
 
 }
