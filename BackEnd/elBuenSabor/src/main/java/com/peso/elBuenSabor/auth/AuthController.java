8c0bf636e2078c6090edba6e727e9a4a9333bfa8
Matias Salinas <matisalinas.2000@gmail.com>
2023-11-20 19:24:12 -0300

    add nueva config CORS

--- BackEnd/elBuenSabor/src/main/java/com/peso/elBuenSabor/auth/AuthController.java
+++ BackEnd/elBuenSabor/src/main/java/com/peso/elBuenSabor/auth/AuthController.java
@@ -13,13 +13,13 @@ public class AuthController {
 
     private final AuthService authService;
 
-    @PostMapping(value = "/login") //no puedo mandar un body con get
+    @PostMapping(path = "/login") //no puedo mandar un body con get
     public ResponseEntity<AuthResponse> login(@RequestBody LoginRequest request)
     {
         return ResponseEntity.ok(authService.login(request));
     }
 
-    @PostMapping(value = "register")
+    @PostMapping(path = "/register")
     public ResponseEntity<AuthResponse> register(@RequestBody RegisterRequest request)
     {
         return ResponseEntity.ok(authService.register(request));
